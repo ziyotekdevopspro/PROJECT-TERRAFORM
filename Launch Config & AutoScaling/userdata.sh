ea2c68cf766d0f5b81459f3e677d7997fa6084e0
ziyotekdevopspro <102825541+ziyotekdevopspro@users.noreply.github.com>
2022-04-27 00:34:49 -0400

    Delete userdata.sh

--- Launch Config & AutoScaling/userdata.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo yum install -y httpd
-cd /var/www/html
-sudo wget https://devops16-project-bucket.s3.amazonaws.com/images/beautiful-landscape.jpg
-sudo wget https://devops16-project-bucket.s3.amazonaws.com/images/index.html
-sudo systemctl enable httpd --now
